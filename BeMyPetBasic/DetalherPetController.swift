0eeda0c5ce5fbc3f1a864629b5f9974cc9d27218
Kassiane Mentz <kassimentz@gmail.com>
2016-10-06 23:14:57 -0300

    add select to choose species

--- BeMyPetBasic/DetalherPetController.swift
+++ BeMyPetBasic/DetalherPetController.swift
@@ -9,27 +9,35 @@
 import UIKit
 import CoreData
 
-class DetalherPetController: UIViewController {
+class DetalherPetController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
+    
     
     weak var managedObjectContext: NSManagedObjectContext?
+    
+
+    @IBOutlet weak var nomeEdit: UITextField!
+    @IBOutlet weak var especieEdit: UITextField!
+    @IBOutlet weak var idadeEdit: UITextField!
+    @IBOutlet weak var pesoEdit: UITextField!
+    @IBOutlet weak var pickerEspecie: UIPickerView!
+    
+    let pickerDataEspecie = ["Gato","Cachorro", "Passarinho","Hamster"]
     
     weak var pet: Pet?{
         didSet{
             updateUI()
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerEspecie.dataSource = self
+        pickerEspecie.delegate = self
         updateUI()
     }
-
-    @IBOutlet weak var nomeEdit: UITextField!
-    @IBOutlet weak var especieEdit: UITextField!
-    @IBOutlet weak var idadeEdit: UITextField!
-    @IBOutlet weak var pesoEdit: UITextField!
     
     private func updateUI(){
+        
         nomeEdit?.text = pet?.nome
         especieEdit?.text = pet?.especie
         
@@ -42,6 +50,26 @@ class DetalherPetController: UIViewController {
         }
     }
 
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataEspecie.count
+    }
+    
+    //MARK: Delegates
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataEspecie[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        especieEdit.text = pickerDataEspecie[row]
+    }
+    
     
     // MARK: Handle Navigation by saving or not
     @IBAction func cancelEdit(_ sender: UIBarButtonItem) {
